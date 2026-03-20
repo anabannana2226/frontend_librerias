0b0c63408306da5b80fee27a9afa0582a3f464a2
anabannana2226 <ana.romo.24s@utzmg.edu.mx>
2026-03-20 13:19:05 -0600

    backend final

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use("/api/libros", librosRoutes);
 app.use('/api/autores', autoresRoutes);
 app.use('/api/usuarios', usuariosRoutes);
 
+app.get('/', (req, res) => {
+    res.send('API de librería funcionando');
+});
+
 app.listen(3000, () => {
     console.log("Servidor corriendo en puerto 3000");
 });
